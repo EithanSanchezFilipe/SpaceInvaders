c3abce7ff426fb025ba35b22666e3ecabc2b0296
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2024-09-02 11:26:40 +0200

    ADD: todo list/ journal de travail

---
+++ IceTools-master/IceTools-master/config.js
@@ -0,0 +1,6 @@
+const iceScrumToken = "25a7d4fa6d5849d5859de72834622f99";
+// 
+const projects = [
+  { projectKey: "SPACEINVAD", cartouche: {} },
+];
+const API_base_url = "https://etml.icescrum.com/ws/project/";
