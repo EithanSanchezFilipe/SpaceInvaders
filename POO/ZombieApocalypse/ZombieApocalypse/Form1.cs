9160e7b29184c0099722fe146e448ed9bdd19a2e
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2024-09-04 10:04:25 +0200

    Add: projet ajouter

---
+++ POO/ZombieApocalypse/ZombieApocalypse/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZombieApocalypse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Console.WriteLine("aaaaaaaaaaaaaaaaaaaaaaaa");
+            pictureBox1.Image = Properties.Resources.zombie;
+
+
+
+        }
+    }
+}
