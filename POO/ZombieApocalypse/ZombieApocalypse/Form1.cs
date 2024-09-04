f1fb4fe5ac0e775f219eff60ef8686d1c7633c57
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2024-09-04 15:38:29 +0200

    essaie de prog (pas reussi)

--- POO/ZombieApocalypse/ZombieApocalypse/Form1.cs
+++ POO/ZombieApocalypse/ZombieApocalypse/Form1.cs
@@ -7,20 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Input;
 
 namespace ZombieApocalypse
 {
     public partial class Form1 : Form
     {
+        private Timer updateTimer;
         public Form1()
         {
             InitializeComponent();
 
-            Console.WriteLine("aaaaaaaaaaaaaaaaaaaaaaaa");
-            pictureBox1.Image = Properties.Resources.zombie;
+            
 
+            updateTimer = new Timer();
+            updateTimer.Interval = 1000;
+            updateTimer.Tick += UpdateTimer_Tick;
+            updateTimer.Start();
+            this.FormClosing += MainForm_FormClosing;
+        }
+        private void UpdateTimer_Tick(object sender, EventArgs e)
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            ConsoleKey key = keyInfo.Key;
+            if (key == ConsoleKey.A)
+            {
 
-
+            }
+            else if (key == ConsoleKey.Escape)
+                Environment.Exit(0);
+        }
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            updateTimer.Stop();    // Stop the timer
+            updateTimer.Dispose(); // Dispose of the timer
         }
     }
 }
