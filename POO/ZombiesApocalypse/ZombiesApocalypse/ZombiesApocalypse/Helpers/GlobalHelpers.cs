76d56ae6294bde9db89a88c0656dbacc4e63693d
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2024-10-02 16:10:02 +0200

    Add(POO):Constantes

--- POO/ZombiesApocalypse/ZombiesApocalypse/ZombiesApocalypse/Helpers/GlobalHelpers.cs
+++ POO/ZombiesApocalypse/ZombiesApocalypse/ZombiesApocalypse/Helpers/GlobalHelpers.cs
@@ -10,12 +10,15 @@ namespace ZombiesApocalypse.Helpers
     {
         public static int SCREENHEIGHT = 900;
         public static int SCREENWIDTH = 700;
-
+        public const float BULLETCOOLDOWN = (float)0.5;
+        public const float ATTACKCHANGECOOLDOWN = (float)10;
+        public const int MINSPAWNDISTANCE = 100;
         public static int RandomNumber(int Min, int Max)
         {
             Random rnd = new Random();
             int NmbrAleatoire = rnd.Next(Min, Max);
             return NmbrAleatoire;
         }
+
     }
 }
