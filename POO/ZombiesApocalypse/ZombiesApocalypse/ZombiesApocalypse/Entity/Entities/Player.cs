65794ea34d68af50e395226f7dbad1d9fbdcc0b9
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2024-10-02 16:12:12 +0200

    feat(POO):changement de sprite en rapport si son attaque spécial est activé ou non et il tire des balles

--- POO/ZombiesApocalypse/ZombiesApocalypse/ZombiesApocalypse/Entity/Entities/Player.cs
+++ POO/ZombiesApocalypse/ZombiesApocalypse/ZombiesApocalypse/Entity/Entities/Player.cs
@@ -9,29 +9,63 @@ namespace ZombiesApocalypse
     class Player : Entity
     {
         private Game _game;
-
-        public Player(Game game, GraphicsDevice graphicsDevice) : base(graphicsDevice)
+        private float _bulletCooldown;
+        private bool _specialAttack;
+        private float _specialAttackCooldown;
+        public Player(Game game) : base()
         {
             _game = game;
+            TintColor = Color.White;
+            Speed = 5;
             Health = 100;
             EntityManager.Add(this);
         }
 
         public override void LoadContent()
         {
-            Console.WriteLine("Loading Texture");
             EntityTexture = _game.Content.Load<Texture2D>("soldat pistolet");
+
+            //Position du joueur declarer ici car la taille de la texture est defini juste au dessus
+            Position = new Vector2(GlobalHelpers.SCREENWIDTH / 2 - EntityTexture.Width / 2, 800);
         }
 
         public override void Update(GameTime gameTime)
         {
-            Velocity = Speed * Velocity;
+            //Mouvement
+            Velocity = Vector2.Zero;
+
             if (InputHelper.GetKeyStatus().IsKeyDown(Keys.A))
                 Velocity = new Vector2(-1, 0);
             if (InputHelper.GetKeyStatus().IsKeyDown(Keys.D))
                 Velocity = new Vector2(1, 0);
+
+            Velocity = Speed * Velocity;
             Position += Velocity;
-            Position = Vector2.Clamp(Position, new Vector2 (0 , Position.Y), new Vector2(GlobalHelpers.SCREENWIDTH - EntityTexture.Width, Position.Y));
+
+            Position = Vector2.Clamp(Position, new Vector2(0, Position.Y), new Vector2(GlobalHelpers.SCREENWIDTH - EntityTexture.Width, Position.Y));
+
+            //Attaque
+            _bulletCooldown -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (InputHelper.GetKeyStatus().IsKeyDown(Keys.Space) &&  _bulletCooldown <= 0)
+            {
+                new Bullet(_game, Position);
+                _bulletCooldown = GlobalHelpers.BULLETCOOLDOWN;
+            }
+
+            //Changement de type d'attaque
+            _specialAttackCooldown -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (InputHelper.GetKeyStatus().IsKeyDown(Keys.X) && _specialAttackCooldown <= 0)
+            {
+                _specialAttack = !_specialAttack;
+                if(_specialAttack)
+                    EntityTexture = _game.Content.Load<Texture2D>("soldat ak");
+                else
+                    EntityTexture = _game.Content.Load<Texture2D>("soldat pistolet");
+                _specialAttackCooldown = GlobalHelpers.ATTACKCHANGECOOLDOWN;
+            }
+
         }
     }
 
