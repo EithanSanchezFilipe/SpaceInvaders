d36f7842dc3cb7b54f4e80e26b31dff1d10c5b81
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2024-10-02 16:09:44 +0200

    Add(POO): class niveau qui fait apparaitre des zombies

---
+++ POO/ZombiesApocalypse/ZombiesApocalypse/ZombiesApocalypse/Level.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using ZombiesApocalypse.Helpers;
+
+namespace ZombiesApocalypse
+{
+    public class Level
+    {
+        private Game _game;
+        private int _numberOfZombiesToSpawn;
+        public int NumberLevel{get; private set;}
+        public int NumberOfZombies { get; private set; }
+
+        public Level(Game game)
+        {
+            _game = game;
+            NumberLevel = 0;
+            NumberOfZombies = 0;
+            _numberOfZombiesToSpawn = 5;
+        }
+
+        public void SpawnZombie()
+        {
+            Vector2 SpawnPosition = Vector2.Zero;
+
+            //Boucle qui créer des zombies jusqu'a sa limite
+            for (int i = 0; i < _numberOfZombiesToSpawn; i++)
+            {
+                //Boucle infinie tant que la position du zombie n'est pas bonne
+                while (true)
+                {
+                    int spawnX = GlobalHelpers.RandomNumber(100, GlobalHelpers.SCREENWIDTH - 100);
+                    int spawnY = GlobalHelpers.RandomNumber(-800, -100);
+                    SpawnPosition = new Vector2(spawnX, spawnY);
+
+                    if (PositionOK(SpawnPosition))
+                    {
+                        new Ennemy(_game, SpawnPosition);
+                        NumberOfZombies++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        private bool PositionOK(Vector2 SpawnPosition)
+        {
+            foreach(Entity entity in EntityManager.Entities)
+            {
+                //Prends que les entités qui sont des zombies
+                if(entity is Ennemy)
+                {
+                    //Calcule la distance entre zombies
+                    float distance = Vector2.Distance(SpawnPosition, entity.Position);
+
+                    if (distance < GlobalHelpers.MINSPAWNDISTANCE)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
