780daee0a84be5d163b5240cfec071d64cd9766c
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2024-10-02 10:09:59 +0200

    add(POO); class player

---
+++ POO/ZombiesApocalypse/ZombiesApocalypse/ZombiesApocalypse/Entity/Entities/Player.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ZombiesApocalypse
+{
+    public class Player : Entity :  DrawableGameComponent
+    {
+        public Player(Game game) : base(game)
+        {
+
+        }
+
+        protected override void LoadContent()
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+
+            base.Draw(gameTime);
+        }
+    }
+}
