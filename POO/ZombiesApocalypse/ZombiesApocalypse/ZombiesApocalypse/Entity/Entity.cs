5c70ffb47337fbd165a6eea5ecc7aed551d03b9c
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2024-10-02 10:09:37 +0200

    add(POO): class entity

---
+++ POO/ZombiesApocalypse/ZombiesApocalypse/ZombiesApocalypse/Entity/Entity.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ZombiesApocalypse
+{
+    abstract class Entity
+    {
+        protected Texture EntityTexture;
+        protected Color TintColor;
+
+        public Vector2 Position;
+        public Vector2 Velocity;
+        public int Health;
+    }
+}
