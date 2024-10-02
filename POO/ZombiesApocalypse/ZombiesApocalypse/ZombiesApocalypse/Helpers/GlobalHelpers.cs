b30a4d2d578fcaebe14aebece30e162d34f2d377
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2024-10-02 10:10:38 +0200

    add(POO): Helpers

---
+++ POO/ZombiesApocalypse/ZombiesApocalypse/ZombiesApocalypse/Helpers/GlobalHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZombiesApocalypse.Helpers
+{
+    static class GlobalHelpers
+    {
+        public static int SCREENHEIGHT = 900;
+        public static int SCREENWIDTH = 700;
+    }
+}
