a130897b5c4a6c865152aa7c0d4940dafae0f5d7
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2024-10-09 16:22:23 +0200

    fix(poo) commentaire et placements

--- POO/ZombiesApocalypse/ZombiesApocalypse/ZombiesApocalypse/Text.cs
+++ POO/ZombiesApocalypse/ZombiesApocalypse/ZombiesApocalypse/Text.cs
@@ -13,8 +13,8 @@ namespace ZombiesApocalypse
         }
         public static void Draw(SpriteBatch spriteBatch )
         {
-            //affichage du niveau (les espaces sont la car sinon le mot level est mal afficher)
-            spriteBatch.DrawString(_font, "Lev e l : " + Level.NumberLevel, new Vector2(0, 30), Color.White);
+            //affichage du niveau (les espaces sont la car sinon le mot level est mal affiché)
+            spriteBatch.DrawString(_font, "Lev e l : " + Level.NumberLevel, new Vector2(30, 30), Color.White);
         }
     }
 }
