43567fcbd601e4cb04eba58cf17fb6b98f19745c
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2024-10-11 10:12:56 +0200

    feat(poo) création de la classe pour ma limite(ou le personnage prends des dégâts)

---
+++ POO/ZombiesApocalypse/ZombiesApocalypse/ZombiesApocalypse/Entity/Entities/Limit.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using ZombiesApocalypse.Helpers;
+namespace ZombiesApocalypse
+{
+    class Fence : Entity
+    {
+        private Game _game;
+        public Fence(Game game, Vector2 PlayerPosition) : base()
+        {
+            _game = game;
+            TintColor = Color.White;
+            Health = 10;
+            Position = new Vector2(PlayerPosition.X, GlobalHelpers.SCREENHEIGHT / 2);
+            EntityManager.Add(this);
+        }
+        public override void LoadContent()
+        {
+            EntityTexture = _game.Content.Load<Texture2D>("Fence");
+            Hitbox = new Rectangle((int)Position.X, (int)Position.Y, EntityTexture.Width, EntityTexture.Height);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+        public void takeDamage(int Damage)
+        {
+            Health -= Damage;
+        }
+    }
+}
