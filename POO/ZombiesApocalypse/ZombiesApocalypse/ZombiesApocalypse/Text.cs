208fef783aec7d7795e0de7699f7e60e36715f50
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2024-10-25 23:36:10 +0200

    fix(POO) mauvais fichier de classe

--- POO/ZombiesApocalypse/ZombiesApocalypse/ZombiesApocalypse/Text.cs
+++ POO/ZombiesApocalypse/ZombiesApocalypse/ZombiesApocalypse/Text.cs
@@ -1,20 +1,34 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 
 namespace ZombiesApocalypse
 {
     public static class Text
     {
         private static SpriteFont _font;
+
         public static void LoadContent(Game game)
         {
             _font = game.Content.Load<SpriteFont>("File");
         }
+
         public static void Draw(SpriteBatch spriteBatch)
         {
-            //affichage du niveau (les espaces sont la car sinon le mot level est mal affiché)
             spriteBatch.DrawString(_font, "Lev e l : " + Level.NumberLevel, new Vector2(30, 30), Color.White);
+        }
+
+        public static void DrawLevelText(SpriteBatch spriteBatch, string text, Vector2 position)
+        {
+            Vector2 textSize = _font.MeasureString(text);
+            Vector2 centeredPosition = position - textSize / 2;
+            spriteBatch.DrawString(_font, text, centeredPosition, Color.Yellow);
+        }
+
+        public static void DrawLoseMessage(SpriteBatch spriteBatch, string text, Vector2 position)
+        {
+            Vector2 textSize = _font.MeasureString(text);
+            Vector2 centeredPosition = position - textSize / 2;
+            spriteBatch.DrawString(_font, text, centeredPosition, Color.White);
         }
     }
 }
