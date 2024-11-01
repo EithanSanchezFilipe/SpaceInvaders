58f837831fea453fedfb0fffc5b6ec288ce7d9b3
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2024-11-01 15:15:55 +0100

    fix(gitignore): correction du fichier gitignore

--- IceTools-master/IceTools-master/config.js
+++
@@ -1,7 +0,0 @@
-const iceScrumToken = "25a7d4fa6d5849d5859de72834622f99";
-// 
-const projects = [
-  { projectKey: "SPACEINVAD", cartouche: {} },
-  { projectKey: "POOSPACEIN", cartouche: {} },
-];
-const API_base_url = "https://etml.icescrum.com/ws/project/";
